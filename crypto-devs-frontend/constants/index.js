3eaea11e5743a082b1a539f04ff5ef52dc121d32
simidawalang <simidawalang@gmail.com>
2022-04-26 12:03:16 +0100

    setup whitelist

--- crypto-devs-frontend/constants/index.js
+++ crypto-devs-frontend/constants/index.js
@@ -1,1 +1,66 @@
 export const WHITELIST_ADDRESS = "0x8BAa9f35546eD0a05Cc034F654270cea3ef28521";
+export const ABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxWhitelistedAddresses",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddressesWhitelisted",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
--- crypto-devs-frontend/pages/index.tsx
+++ crypto-devs-frontend/pages/index.tsx
@@ -1,18 +1,101 @@
 import React, { useEffect, useState, useRef } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Web3Modal from "web3modal";
+import { Contract, providers } from "ethers";
 import Image from "next/image";
-import abi from "../../ethereum/artifacts/contracts/Whitelist.sol/Whitelist.json";
 import headerImg from "../assets/crypto-devs.svg";
 import Button from "../components/button";
+import { WHITELIST_ADDRESS, ABI } from "../constants/index";
 
 const Home: NextPage = () => {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
-  const [isWHitelisted, setIsWhitelisted] = useState(false);
+  const [isWhitelisted, setIsWhitelisted] = useState(false);
   const [loading, setIsLoading] = useState(false);
   const [noOfWhitelisted, setNoOfWhitelisted] = useState(0);
-  const web3Modal = useRef();
-  
+  const web3ModalRef: any = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Use Rinkeby");
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const addAddressToWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(WHITELIST_ADDRESS, ABI, signer);
+      const tx = await whitelistContract.addAddressToWhitelist();
+      setIsLoading(true);
+      await tx.wait();
+      setIsLoading(false);
+
+      await getNumberOfWhitelisted();
+      setIsWhitelisted(true);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getNumberOfWhitelisted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+
+      const whitelistContract = new Contract(WHITELIST_ADDRESS, ABI, provider);
+
+      const _noOfWhitelisted =
+        await whitelistContract.numAddressesWhitelisted();
+      setNoOfWhitelisted(_noOfWhitelisted);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const checkIfWhitelisted = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+
+      const whitelistContract = new Contract(WHITELIST_ADDRESS, ABI, signer);
+      const address = await signer.getAddress();
+      const joinedWhitelist = await whitelistContract.whitelistedAddresses(
+        address
+      );
+      setIsWhitelisted(!!joinedWhitelist);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setIsWalletConnected(true);
+      checkIfWhitelisted();
+      getNumberOfWhitelisted();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    if (!isWalletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [isWalletConnected]);
+
   return (
     <div>
       <Head>
@@ -28,15 +111,30 @@ const Home: NextPage = () => {
           <div className="homepage-header__info">
             <h1 className="homepage-header__info-title">CryptoDevs</h1>
             <p className="homepage-header__info-text">
-              An NFT collection for developers in crypto. <br /> 3 have already
+              An NFT collection for developers in crypto. <br /> {noOfWhitelisted} have already
               joined.
             </p>
-            <Button
-              className="homepage-header__btn"
-              content="Join the whitelist"
-              variant="green"
-              size="lg"
-            />
+            {isWalletConnected ? (
+              isWhitelisted ? (
+                <h3>Thanks for joining the whitelist!</h3>
+              ) : (
+                <Button
+                  className="homepage-header__btn"
+                  content="Join the whitelist"
+                  variant="green"
+                  size="lg"
+                  onClick={addAddressToWhitelist}
+                />
+              )
+            ) : (
+              <Button
+                className="homepage-header__btn"
+                content="Connect Wallet"
+                variant="green"
+                size="lg"
+                onClick={connectWallet}
+              />
+            )}
           </div>
           <div className="homepage-header__img">
             <Image src={headerImg} alt="CryptoDevs header image placeholder" />
